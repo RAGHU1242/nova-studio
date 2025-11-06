56191ccfdd815a0310f3438ab184e606a21fd35a
RAGHU1242 <m.raghavendra331@gmail.com>
2025-11-06 12:21:22 +0530

    feat: add client/App.tsx

---
+++ client/App.tsx
@@ -0,0 +1,37 @@
+import "./global.css";
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import BattleArena from "./pages/BattleArena";
+import Leaderboard from "./pages/Leaderboard";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/battle" element={<BattleArena />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
