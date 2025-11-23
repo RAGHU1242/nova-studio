6eba884d774dfbe08d495d50ce5523c401c278de
RAGHU1242 <m.raghavendra331@gmail.com>
2025-11-23 10:40:00 +0530

    fix(ui): update collapsible component

--- client/components/ui/collapsible.tsx
+++ client/components/ui/collapsible.tsx
@@ -1,8 +1,20 @@
 /* --- THEME UPGRADED COLLAPSIBLE --- */
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+import { cn } from "@/lib/utils";
 
 const Collapsible = CollapsiblePrimitive.Root;
+
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+
+const CollapsibleContent = ({ className, ...props }: React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.CollapsibleContent>) => (
+    <CollapsiblePrimitive.CollapsibleContent 
+        className={cn(
+            "overflow-hidden transition-all duration-300 ease-in-out",
+            "data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down",
+            className
+        )}
+        {...props}
+    />
+);
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent };
